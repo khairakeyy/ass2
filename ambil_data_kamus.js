1cdfa7ea5ca872cb3d012a55f890512cb6ed595c
khairakeyy <hutamikhairatul@gmail.com>
2024-05-06 11:19:39 +0700

    Mengambil Data JSON Menggunakan AJAX

--- ambil_data_kamus.js
+++ ambil_data_kamus.js
@@ -1,36 +1,11 @@
-// Panggil data dari server ke dalam bentuk JSON menggunakan AJAX
 $(document).ready(function(){
     $.ajax({
-        url: "ambil_data_kamus.php",
+        url: "ambil_data_jadwal.php",
         type: "GET",
         dataType: "json",
         success: function(data){
-                // Proses hasil pemanggilan AJAX
-            // Tampilkan data ke dalam bentuk tabel
-            var tabel = "<table><tr><th>ID</th><th>Kata Isyarat</th><th>Terjemahan</th><th>Aksi</th></tr>";
-            $.each(data, function(index, kata){
-                tabel += "<tr>";
-                tabel += "<td>" + kata.id + "</td>";
-                tabel += "<td>" + kata.kata_isyarat + "</td>";
-                tabel += "<td>" + kata.terjemahan + "</td>";
-                tabel += "<td><button onclick='hapusData(" + kata.id + ")'>Hapus</button></td>";
-                tabel += "</tr>";
-            });
-            tabel += "</table>";
-            $("#tabel_kamus").html(tabel);
+            // Panggil fungsi untuk menampilkan data
+            tampilkanData(data);
         }
     });
 });
-
-// Fungsi untuk menghapus data dengan menggunakan AJAX
-function hapusData(id_kata){
-    $.ajax({
-        url: "hapus_data.php?id=" + id_kata,
-        type: "GET",
-        success: function(){
-            // Refresh halaman atau hapus baris tabel
-            // Untuk kesederhanaan, Anda bisa memuat ulang halaman
-            location.reload();
-        }
-    });
-}
