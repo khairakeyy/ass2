7580745c30f8461fd14cf14e3451830464a8d4ef
khairakeyy <hutamikhairatul@gmail.com>
2024-05-06 11:44:42 +0700

    perbaikan

--- ambil_data_bahasa_isyarat.php
+++ ambil_data_bahasa_isyarat.php
@@ -5,21 +5,21 @@ if (!$koneksi) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 
-$query = "SELECT * FROM bahasa_isyarat";
+$query = "SELECT * FROM tabel_bahasa_isyarat";
 $result = mysqli_query($koneksi, $query);
 
-$data_bahasa_isyarat = array();
+$tabel_bahasa_isyarat = array();
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $data_bahasa_isyarat[] = $row;
+        $tabel_bahasa_isyarat[] = $row;
     }
 } else {
     echo "Tidak ada data bahasa isyarat.";
 }
 
 header('Content-Type: application/json');
-echo json_encode($data_bahasa_isyarat);
+echo json_encode($tabel_bahasa_isyarat);
 
 mysqli_close($koneksi);
 ?>
