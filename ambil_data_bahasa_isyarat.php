73dadb06124ffffa11cf1e4355a96ae0ce1957e9
khairakeyy <hutamikhairatul@gmail.com>
2024-05-11 16:15:57 +0700

    update

--- ambil_data_bahasa_isyarat.php
+++ ambil_data_bahasa_isyarat.php
@@ -1,25 +1,25 @@
 <?php
-$koneksi = mysqli_connect("localhost", "username", "password", "bahasaisyarat");
+$koneksi = mysqli_connect("localhost", "username", "password", "nama_database");
 
 if (!$koneksi) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 
-$query = "SELECT * FROM tabel_bahasa_isyarat";
+$query = "SELECT * FROM bahasaisyarat";
 $result = mysqli_query($koneksi, $query);
 
-$tabel_bahasa_isyarat = array();
+$bahasaisyarat = array();
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $tabel_bahasa_isyarat[] = $row;
+        $bahasaisyarat[] = $row;
     }
 } else {
     echo "Tidak ada data bahasa isyarat.";
 }
 
 header('Content-Type: application/json');
-echo json_encode($tabel_bahasa_isyarat);
+echo json_encode($bahasaisyarat);
 
 mysqli_close($koneksi);
 ?>
